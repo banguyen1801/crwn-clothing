91b327ca0a05ed31a936169079491e62792f6021
Ba Nguyen <b.pynguyen@gmail.com>
2020-06-05 23:07:50 -0400

    factor code of homepage, directory, menu-item component and finish styling them

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Directory } from '../../components/directory/directory.component';
+
+import './homepage.styles.scss';
+
+export const HomePage = (props) => (
+  <div className="homepage">
+    <Directory></Directory>
+  </div>
+);
+
+// export default HomePage;
